3a020445406abfa964c0c5767688e6495064d62d
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-26 21:07:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(collection)
+  if block_given?
+    i = 0 
+    
+    while i < collection.length 
+      new_collection = []
+      yield collection[i]
+      i = i + 1
+      new_collection << a.values_at[i]
+    end
+    
+    new_collection
+  else
+    "Hey! No block was given!"
+  end
+end
